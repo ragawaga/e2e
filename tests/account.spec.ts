3000941f61e0e37749578b5dc580d3050ca994e3
Gary Tierney <gary.tierney@fastmail.com>
2023-03-08 17:19:48 +0000

    Add GHA workflow and account test

---
+++ tests/account.spec.ts
@@ -0,0 +1,23 @@
+import { expect, test } from "@playwright/test";
+import { asAuthenticatedUser } from "./auth";
+
+test.describe("a logged in user with full permissions", () => {
+  asAuthenticatedUser();
+
+  test("should see their account settings", async ({ page }) => {
+    await page.goto("/account-settings");
+
+    await expect(page.getByTestId("account-details")).toBeVisible();
+
+    for (const [field, value] of [
+      ["name", "Cru Testuser11"],
+      ["email", "Cruonlinetestuser11@gmail.com"],
+      ["company", "CRU International"],
+      ["job", "Research Analyst"],
+    ]) {
+      await expect(page.getByTestId(`account-${field}`)).toContainText(value);
+    }
+
+    await expect(page).toHaveScreenshot();
+  });
+});
