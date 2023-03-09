cb609ff3a39d1e95f1e861ccf29c7510180705ba
Gary Tierney <gary.tierney@fastmail.com>
2023-03-09 10:31:00 +0000

    Add Azure pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [
+    ["html"],
+    ["junit", { outputFile: "test-results/e2e-junit-results.xml" }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
