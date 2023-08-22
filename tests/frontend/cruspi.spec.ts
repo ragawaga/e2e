34423e6c02f9a46e1be623991d1c8000d7b6b7ce
Christine MacDonald <christine.macdonald@digirati.com>
2023-08-22 15:06:34 +0100

    Migrated cruspi tests

---
+++ tests/frontend/cruspi.spec.ts
@@ -0,0 +1,86 @@
+import { expect, test} from "@playwright/test";
+import { globalLocators } from "./pages/GlobalLocators";
+
+test.describe("CRUSpi Prices Page", () => {
+
+  test("can navigate straight to the correct URL", async ({ page }) => {
+    await page.goto(globalLocators.cruSpiPricesPageURL);
+    expect(await page.locator(globalLocators.header).textContent()).toEqual(globalLocators.cruspiString);
+  });
+
+  test("can navigate to cruspi prices page via the Prices menu", async ({ page }) => {
+    await page.goto("/analysis");
+    //TODO Somehow move to Global as we will need to access the prices menu often
+    await page.getByRole('button', { name: 'Prices', exact: true }).hover();
+    await page.getByRole('link', { name: globalLocators.cruspiString }).click();
+    
+    expect(await page.locator(globalLocators.header).textContent()).toEqual(globalLocators.cruspiString);
+  });
+
+  test("there are no tabs on the CRUSpi page and page structure is as expected", async ({ page }) => {
+    await page.goto("/prices/cruspi");
+    expect( await page.locator(globalLocators.header).textContent()).toEqual(globalLocators.cruspiString);
+    await expect( page.getByText('Price Indicators', { exact: true })).toBeVisible();
+
+    //Check that there are no tabs on this Price page - only Price page with no tabs
+    //expect(await page.getByRole('tablist').count()).toBe(0); 
+    await expect(page.getByRole('tablist')).not.toBeAttached();
+
+    //Check the Article content is there
+    await expect(page.getByText('Price Indicators', { exact: true })).toBeVisible();
+
+    //Check the downloads button is there
+    await expect(page.getByTestId('prices-download-button')).toBeVisible();
+
+    //Check that the Prices table is there and that Check the chart is visible
+    //There will be 2 occurances of 'Global Flat products Price Indicator', one 
+    //in the table and one in the chart
+    await expect(page.getByTestId('prices-table').getByText('Global Flat products Price Indicator')).toBeVisible();
+    await expect(page.getByTestId('cruspi-chart').getByText('Global Flat products Price Indicator')).toBeVisible();
+  
+    //Click the Price notice button and check it takes you to the correct pages
+    await page.getByTestId('price-notices-button').click();
+    await expect(page).toHaveURL(/notifications*/);
+
+  });
+});
+
+test.describe("CRUSpi Downloads Page", () => {
+
+  test("Navigate to CRUspi downloads page", async ({ page }) => {
+
+    //Navigate directly the known URL
+    await page.goto(globalLocators.cruSpiDownloadsPageURL);
+    expect( await page.locator(globalLocators.header).textContent()).toEqual(globalLocators.cruspiString);
+
+    //Check navigating via the Downloads Menu works (had issues in the past where CRUspi disappeared from the menu)
+    await page.getByRole('link', { name: 'Downloads', exact: true }).click();
+    
+
+    //Check we are on the Download index page and CRUspi exists
+    await expect(page).toHaveURL(globalLocators.downloadsPageURL);
+    expect( await page.locator(globalLocators.header).textContent()).toEqual(globalLocators.downloadsPageTitle);
+    
+
+    //Click through to the CRUspi downloads page
+    const cruspiLink = page.getByTestId('download-category__0-list').getByRole('link', { name: globalLocators.cruspiString });
+    await cruspiLink.isVisible();
+    await cruspiLink.scrollIntoViewIfNeeded();
+    await cruspiLink.click();
+    
+    expect(await page.locator(globalLocators.header).textContent()).toEqual(globalLocators.cruspiString);
+
+    //Check downloads links are there, and working
+    //Use Feb 2022 as this file should always be there
+    await page.getByRole('button', { name: 'February 2022' }).click();
+    await page.getByRole('heading', { name: 'February 2022' }).isVisible();
+
+    //Check the download link exists and works .
+    const excelFileButton = page.getByRole('button', { name: 'CRUspi 1994-2023 (10 02 2022).xlsx', exact: true });
+    const downloadPromise = page.waitForEvent('download');
+    await excelFileButton.hover();
+    excelFileButton.click();
+    const download = await downloadPromise;
+    expect (await download.path()).not.toBeNull;
+  });
+})
--- tests/frontend/pages/GlobalLocators.ts
+++ tests/frontend/pages/GlobalLocators.ts
@@ -1,4 +1,9 @@
 export const globalLocators = {
     header: "h1",
-    userMenu: { role: 'button', name: 'John Smith' }
+    userMenu: { role: 'button', name: 'John Smith' },
+    cruspiString: 'CRUspi',
+    cruSpiPricesPageURL: '/prices/cruspi',
+    cruSpiDownloadsPageURL: 'downloads/monitor/cruspi',
+    downloadsPageURL: '/downloads',
+    downloadsPageTitle: 'Downloads'
 }
