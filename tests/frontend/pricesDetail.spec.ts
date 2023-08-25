ee1f044f0c1839eb20477df6ad76196ecff5c78c
funkydunc <duncan.saunders@digirati.com>
2023-08-25 11:38:43 +0100

    pricesDetail xls download tests wip

--- tests/frontend/pricesDetail.spec.ts
+++ tests/frontend/pricesDetail.spec.ts
@@ -12,13 +12,12 @@ test.describe("Prices detail pages", () => {
     
     // Check the title
     await expect(pricesDetail.h1).toHaveText(/Ferromanganese EU MC/);
-    
-    await expect(pricesDetail._data_testid_content_heading_.first())
+    await expect(pricesDetail.contentHeading.first())
       .toHaveText("Latest Prices");
 
     // There should be 2 price cards.
-    await expect(pricesDetail._data_testid_prices_card_).toHaveCount(2);
-    await expect(pricesDetail._data_testid_prices_table_).toHaveCount(2);
+    await expect(pricesDetail.pricesCard).toHaveCount(2);
+    await expect(pricesDetail.pricesTable).toHaveCount(2);
   });
 
   test("Display Optical fibre price detail page", async ({ pricesDetail, page }) => {
@@ -26,12 +25,26 @@ test.describe("Prices detail pages", () => {
     
     // Check the title
     await expect(pricesDetail.h1).toHaveText(/Bare Fibre/);
-
-    await expect(pricesDetail._data_testid_content_heading_.first())
+    await expect(pricesDetail.contentHeading.first())
       .toHaveText("Latest Prices");
 
     // There should only be 1 price card.
-    await expect(pricesDetail._data_testid_prices_card_).toHaveCount(1);
-    await expect(pricesDetail._data_testid_prices_table_).toHaveCount(1);
+    await expect(pricesDetail.pricesCard).toHaveCount(1);
+    await expect(pricesDetail.pricesTable).toHaveCount(1);
   });
+
+  // Test xlsx download
+  test("Download chart XLS", async ({ pricesDetail, page }) => {
+    await page.goto("/price/1");
+    await expect(pricesDetail.chartDlMenu).toBeInViewport()
+    //const downloadPromise = page.waitForEvent('download');
+    await pricesDetail.chartDlMenu.first().click();
+    await expect(pricesDetail.downloadXLS).toBeInViewport()
+    //await pricesDetail.downloadXLS.first().click()
+    //const download = await downloadPromise;
+    //const path = (await download.path());
+    // expect(path).toContain(/Urea Prilled Bulk FOB Black Sea Spot/);
+    // expect(path).toContain(/.xlsx/);
+  });
+
 });
