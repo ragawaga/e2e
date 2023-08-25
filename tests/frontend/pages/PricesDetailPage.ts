4f3c1229f49fbecea541ef1a3068fe917c3e5913
funkydunc <duncan.saunders@digirati.com>
2023-08-25 11:38:43 +0100

    pricesDetail xls download tests wip

--- tests/frontend/pages/PricesDetailPage.ts
+++ tests/frontend/pages/PricesDetailPage.ts
@@ -4,9 +4,11 @@ import { createComponentLocators } from "../../component";
 export function pricesDetailPageModel(page: Page) {
   const screen = createComponentLocators(page, {
     h1: "h1",
-    _data_testid_content_heading_: {testId: "content-heading"},
-    _data_testid_prices_card_: {testId: "prices-card"},
-    _data_testid_prices_table_: {testId:"prices-table"}
+    contentHeading: {testId: "content-heading"},
+    pricesCard: {testId: "prices-card"},
+    pricesTable: {testId:"prices-table"},
+    chartDlMenu: {role: 'button', name: /view chart menu/},
+    downloadXLS: {text: /download xls/}
 });
 
   return {
