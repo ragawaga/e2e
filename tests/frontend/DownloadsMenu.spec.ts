0db8358de9487c83b5d5b0d612a2f2c3ac8c4107
brianmac1967 <brian.mac1967.00@gmail.com>
2023-09-04 17:41:07 +0100

    feedback fixes

--- tests/frontend/DownloadsMenu.spec.ts
+++ tests/frontend/DownloadsMenu.spec.ts
@@ -5,60 +5,44 @@ import { createTestFixture } from "../fixture";
 
 const test = createTestFixture("downloadsMenuModel", DownloadsMenuPageModel);
 
-const downloadsHref = "#nav-menu-downloads";
-const downloadGroupList = '.downloads-group__list li a';
-const downloadGroupListItemTitle ='.chw-archives-list__item__title';
-
 test.describe("Downloads Page", () => {
 
-  test("Dowloads menu item should exist", async ({ page,  downloadsMenuModel}) => {
-    await downloadsMenuModel.load();
+  test("Downloads menu item should exist", async ({ page, downloadsMenuModel }) => {
     await page.goto("/");
-    await expect(page.locator(downloadsHref)).toHaveCount(1); 
+    await expect(downloadsMenuModel.downloadsHref).toBeVisible();
   });
 
   test("Navigate to Downloads page", async ({ page, layout }) => {
+     await page.goto("/downloads");
      await expect (layout.header).toHaveText(GlobalConstants.downloadsPageTitle);
   
      await layout.downloadsTopNav.click();
-  
      await expect(page).toHaveURL(GlobalConstants.downloadsPageURL);
      await expect(layout.header).toHaveText(GlobalConstants.downloadsPageTitle);
   
   });
 
-   test("Direct Navigation to downloads", async ({ page, downloadsMenuModel }) => {
-     await downloadsMenuModel.load();
+  test("Direct Navigation to downloads", async ({ page, downloadsMenuModel }) => {
      await page.goto("/downloads");
-
-     //banner contains 
-     await expect(page.locator(downloadsHref)).toHaveCount(1); 
+     await expect(downloadsMenuModel.downloadsHref).toBeVisible();
      await page.locator(downloadsMenuModel.chwPageBanner + " " + downloadsMenuModel.header + ':text("Downloads")');
-
    });
 
   test("Download Item are available", async ({ page, downloadsMenuModel }) => {
-     await downloadsMenuModel.load();
      await page.goto("/downloads");
-     await expect(
-       downloadsMenuModel.downloadCategory0,
-     ).toBeAttached();
+     await expect(downloadsMenuModel.downloadCategory0).toBeVisible();
    });
 
   test("Click through get to downloads product page", async ({ page, downloadsMenuModel }) => {
-     await downloadsMenuModel.load();
      await page.goto("/downloads");
-
-     await expect(page.locator(downloadGroupList).first()).toHaveCount(1); 
-     await page.locator(downloadGroupList).first().click();
+     await downloadsMenuModel.downloadGroupList.first().click();
      await expect(page).toHaveURL(new RegExp("/downloads/monitor"));
    });
 
-   test("Downloads Edition Page contains Download files", async ({ page }) => {
+   test("Downloads Edition Page contains Download files", async ({ page, downloadsMenuModel }) => {
     await page.goto("/downloads");
-    await page.locator(downloadGroupList).first().click();
-
-    await expect(page.locator(downloadGroupListItemTitle).first()).toHaveCount(1); 
+    await downloadsMenuModel.downloadGroupList.first().click();
+    await expect(downloadsMenuModel.downloadGroupListItemTitle.first()).toBeVisible();
   });
 
 });
--- tests/frontend/pages/DownloadsMenuPage.ts
+++ tests/frontend/pages/DownloadsMenuPage.ts
@@ -1,30 +1,14 @@
 import { Page } from "@playwright/test";
 import { createComponentLocators } from "../../component";
 
-/* export const downloadsMenuConstants = {
-  aHrefDownloads_: "a[href='/downloads']",
-  dataTestId_ChwPageBannerH1: '[data-testid="chw-pagebanner"] h1',
-  dataTestIdDownloadCategory0 : '[data-testid="download-category__0"]',
-  dataTestIdDownloadCategory0LiA: '[data-testid="download-category__0"] li a',
-  dataTestIdChwArchiveSummary : '[data-testid="chw-archive-summary"]',
-
-  dataTestIdChwPageBannerBackground: '[data-testid="chw-pagebanner_background"]',
-  chwArchivesListLabel: ".chw-archives-list__label",
-  chwArchivesListItem: ".chw-archives-list__item" 
-}; */
-
 export function DownloadsMenuPageModel(page: Page) {
   const screen = createComponentLocators(page, {
     header: "h1",
-    listItem: "li",
-    anchor: "a",
     chwPageBanner: { testId: "chw-pagebanner" },
     downloadCategory0: { testId: "download-category__0-list" },
-    ownloadCategory0: { testId: "download-category__0-list" },
-    chwArchiveSummary: { testId: "chw-archive-summary" },
-    chwPageBannerBackground: { testId: "chw-pagebanner_background" },
-    chwArchivesListLabel: ".chw-archives-list__label",
-    chwArchivesListItem: ".chw-archives-list__item" 
+    downloadsHref: "#nav-menu-downloads",
+    downloadGroupList: '.downloads-group__list li a',
+    downloadGroupListItemTitle: '.chw-archives-list__item__title'
   });
 
   return {
