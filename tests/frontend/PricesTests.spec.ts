857b41757775347ad0d11fc93c8af38730808d7c
Christine MacDonald <christine.macdonald@digirati.com>
2023-09-14 22:19:18 +0100

    Moved locators etc to PageModel file

--- tests/frontend/PricesTests.spec.ts
+++ tests/frontend/PricesTests.spec.ts
@@ -1,75 +1,64 @@
 import { expect } from "@playwright/test";
 import { createTestFixture } from "../fixture";
-import { notificationsStreamPageModel } from "./pages/NotificationsStreamPage";
+import { GlobalConstants } from "./pages/Layout";
+import { pricePageConstants, pricesPageModel } from "./pages/PricesPage";
 
-const test = createTestFixture("notificationsStream", notificationsStreamPageModel);
+const test = createTestFixture("prices", pricesPageModel);
 
 test.describe("Prices Tests", () => {
   
-  test("Prices button exists in the top nav", async ({ notificationsStream, page, layout })  => {
-    await page.goto("/analysis");
+  test("Prices button exists in the top nav", async ({ page, layout })  => {
+    await page.goto(GlobalConstants.home);
     await expect(layout.pricesTopNav).toBeVisible(); 
   });
 
-  test("Navigate to price page", async ({ notificationsStream, page, layout })  => {
-    const navTo = `/prices/aluminium`;
-    await page.goto(navTo);
-    await expect(page).toHaveURL(navTo);
-    await expect(layout.header).toHaveText("Aluminium");
+  test("Navigate to price page", async ({ prices, page, layout })  => {
+    prices.load(pricePageConstants.aluminiumURL);
+    await expect(layout.header).toHaveText(pricePageConstants.aluminiumPageHeader);
   });
 
-  test("Can click ranges changes state", async ({ notificationsStream, page, layout })  => {
-    const navTo = `/prices/aluminium`;
-    await page.goto(navTo);
-    await expect(page).toHaveURL(navTo);
+  test("Can click ranges changes state", async ({ prices })  => {
+    prices.load(pricePageConstants.aluminiumURL);
 
     //Check we are on the Overview tab
-    await expect(page.getByRole('tab', {name:'Overview'})).toHaveAttribute("aria-selected", "true");
+    await expect(prices.overviewTab).toHaveAttribute(pricePageConstants.tabActiveAttribute, "true");
 
     //Navigate to the other tabs, check the page changes
-    const weekly = page.getByRole('tab', {name: 'Weekly'});
-    const monthly = page.getByRole('tab', {name: 'Monthly'});
+    await prices.weeklyTab.click();
+    await expect(prices.weeklyTab).toHaveAttribute(pricePageConstants.tabActiveAttribute, "true");
+    await expect(prices.monthlyTab).toHaveAttribute(pricePageConstants.tabActiveAttribute, "false");
+    await expect(prices.downloadTableButton).toBeVisible();
 
-    await weekly.click();
-    await expect(weekly).toHaveAttribute("aria-selected", "true");
-    await expect(monthly).toHaveAttribute("aria-selected", "false");
-    await expect(page.getByRole('button', {name: 'Download table'})).toBeVisible();
-
-    await monthly.click();
-    await expect(monthly).toHaveAttribute("aria-selected", "true");
-    await expect(weekly).toHaveAttribute("aria-selected", "false");
+    await prices.monthlyTab.click();
+    await expect(prices.monthlyTab).toHaveAttribute(pricePageConstants.tabActiveAttribute, "true");
+    await expect(prices.weeklyTab).toHaveAttribute(pricePageConstants.tabActiveAttribute, "false");
+    await expect(prices.downloadTableButton).toBeVisible();
   });
 
-  test("Prices table and download button exists on Weekly and Monthly", async ({ notificationsStream, page, layout })  => {
-    const navTo = `/prices/aluminium`;
-    await page.goto(navTo);
-    await expect(page).toHaveURL(navTo);
+  test("Prices table and download button exists on Weekly and Monthly", async ({ prices })  => {
+    prices.load(pricePageConstants.aluminiumURL);
 
     //Navigate to the weekly tab and check donwload option and the table are there
-    const weekly = page.getByRole('tab', {name: 'Weekly'});
-    await weekly.click();
-    await expect(page.getByTestId("prices-table")).toBeVisible();
-    await expect(page.getByRole('button', {name: 'Download table'})).toBeVisible();
+    await prices.weeklyTab.click();
+    await expect(prices.pricesTable).toBeVisible();
+    await expect(prices.downloadTableButton).toBeVisible();
 
     //Navigate to the monthly tab and check donwload option and the table are there
-    const monthly = page.getByRole('tab', {name: 'Monthly'});
-    await monthly.click();
-    await expect(page.getByTestId("prices-table")).toBeVisible();
-    await expect(page.getByRole('button', {name: 'Download table'})).toBeVisible();
+    await prices.monthlyTab.click();
+    await expect(prices.pricesTable).toBeVisible();
+    await expect(prices.downloadTableButton).toBeVisible();
   });
 
-  test("Fertilizer Week Prices has nutrient filter", async ({ notificationsStream, page, layout })  => {
-    const navTo = `prices/fertilizer-week-base?tab=weekly-prices`;
-    await page.goto(navTo);
-    await expect(page).toHaveURL(navTo);
+  test("Fertilizer Week Prices has nutrient filter", async ({ prices, page, layout })  => {
+    prices.load(pricePageConstants.fwURL);
 
     //Should be on FW page initally
-    await expect(layout.header).toHaveText("Fertilizer Week");
+    await expect(layout.header).toHaveText(pricePageConstants.fwWeekPageHeader);
 
     // Select Urea to filter page content
-    await page.getByTestId("select-prices-nutrient-select").selectOption('Urea');
+    await prices.fwNutrientDropdown.selectOption(pricePageConstants.ureaOption);
 
     // Confirm that the page has changed to the Urea page
-    await expect(layout.header).toHaveText("Urea");
+    await expect(layout.header).toHaveText(pricePageConstants.ureaOption);
   });
 });
