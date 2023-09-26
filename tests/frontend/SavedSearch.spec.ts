baadb37b773329fd643e3fb7a27ccd8cc4b85ebe
brianmac1967 <brian.mac1967.00@gmail.com>
2023-09-26 13:03:20 +0100

    PR feedback to clean up saved seraches after creating and get save button by role and tex

--- tests/frontend/SavedSearch.spec.ts
+++ tests/frontend/SavedSearch.spec.ts
@@ -24,16 +24,30 @@ test.describe("Searches and MyCru Page", () => {
     await expect(savedSearchModel.saveSearchButton).toBeVisible();
     await savedSearchModel.saveSearchButton.click();
 
-    let randomNumber = Math.random().toString().substring(2);
+    let savedSearchSuffix = " saved search_" + Math.random().toString().substring(2);
+
     //see saved search modal, enter text and click Save button
     await expect(savedSearchModel.savedSearchModal).toBeVisible();
-    await savedSearchModel.savedSearchInput.fill(SavedSearchPageConstants.searchTerm + " saved search_" + randomNumber);
-    await page.locator('"Save"').click();
+    await savedSearchModel.savedSearchInput.fill(SavedSearchPageConstants.searchTerm + savedSearchSuffix);
+    await page.getByRole('button').getByText('Save').click();
 
-    await expect(savedSearchModel.savedsearches).toContainText(
-      SavedSearchPageConstants.searchTerm + " saved search_" + randomNumber
-    );
+    await expect(savedSearchModel.savedsearches).toContainText(SavedSearchPageConstants.searchTerm + savedSearchSuffix);
+
+    //clean up the saved search
+    await page.goto("/mycru");
+
+    //go to Searches tab
+    await expect(savedSearchModel.tabSearches).toBeVisible();
+    await savedSearchModel.tabSearches.click();
+
+    await expect(savedSearchModel.savedSearchesArticleTitle.getByText(SavedSearchPageConstants.searchTerm + savedSearchSuffix).first()).toBeVisible();
+    await expect(savedSearchModel.savedSearchesArticleDeleteButton.first()).toBeVisible();
+
+    //delete saved searcg
+    await savedSearchModel.savedSearchesArticleDeleteButton.first().click();
+    await expect(savedSearchModel.savedSearchesArticleTitle.getByText(SavedSearchPageConstants.searchTerm + savedSearchSuffix).first()).not.toBeVisible();
   });
+
 
   test("Can cancel save search", async ({ page, savedSearchModel }) => {
     await page.goto("/search");
@@ -55,6 +69,7 @@ test.describe("Searches and MyCru Page", () => {
     await expect(savedSearchModel.savedSearchModal).not.toBeVisible();
   });
 
+
   test("Can navigate to MyCru searches tab", async ({ page, savedSearchModel }) => {
     await page.goto("/search");
     await expect(savedSearchModel.searchInput).toBeVisible();
@@ -66,17 +81,25 @@ test.describe("Searches and MyCru Page", () => {
     await expect(savedSearchModel.saveSearchButton).toBeVisible();
     await savedSearchModel.saveSearchButton.click();
 
+    let searchNameSuffix = " saved search_" + Math.random().toString().substring(2);
+
     //see saved search modal, enter text and click Save button
     await expect(savedSearchModel.savedSearchModal).toBeVisible();
-    await savedSearchModel.savedSearchInput.fill(SavedSearchPageConstants.searchTerm + " saved search_" + Math.random().toString().substring(2));
-    await page.locator('"Save"').click();
+    await savedSearchModel.savedSearchInput.fill(SavedSearchPageConstants.searchTerm + searchNameSuffix);
+    await page.getByRole('button').getByText('Save').click();
 
     await page.goto("/mycru");
 
     await expect(savedSearchModel.tabSearches).toBeVisible();
     await savedSearchModel.tabSearches.click();
     await expect(savedSearchModel.savedSearchesList).toBeVisible();
+
+    //clean up saved search
+    await expect(savedSearchModel.savedSearchesArticleDeleteButton.first()).toBeVisible();
+    await savedSearchModel.savedSearchesArticleDeleteButton.first().click();
+    await expect(page.getByText(SavedSearchPageConstants.searchTerm + searchNameSuffix)).not.toBeVisible();
   });
+
 
   test("Saved searches is a list of clickable links", async ({ page, savedSearchModel }) => {
     await page.goto("/mycru");
@@ -87,6 +110,7 @@ test.describe("Searches and MyCru Page", () => {
     //expect search item to be visible
     await expect(savedSearchModel.savedSearchesArticleTitle.first()).toBeVisible();
   });
+
 
   test("Can delete a saved search", async ({ page, savedSearchModel }) => {
     await page.goto("/search");
@@ -99,11 +123,11 @@ test.describe("Searches and MyCru Page", () => {
     await expect(savedSearchModel.saveSearchButton).toBeVisible();
     await savedSearchModel.saveSearchButton.click();
 
-    let randomNumber = Math.random().toString().substring(2);
+    let savedSearchNameSuffix = " saved search_" + Math.random().toString().substring(2);
     //see saved search modal, enter text and click Save button
     await expect(savedSearchModel.savedSearchModal).toBeVisible();
-    await savedSearchModel.savedSearchInput.fill(SavedSearchPageConstants.searchTerm + " saved search_" + randomNumber);
-    await page.locator('"Save"').click();
+    await savedSearchModel.savedSearchInput.fill(SavedSearchPageConstants.searchTerm + savedSearchNameSuffix);
+    await page.getByRole('button').getByText('Save').click();
 
     await page.goto("/mycru");
     await expect(savedSearchModel.tabSearches).toBeVisible();
@@ -111,25 +135,39 @@ test.describe("Searches and MyCru Page", () => {
     await expect(savedSearchModel.savedSearchesList).toBeVisible();
 
     //expect search item to be visible
-    await expect(savedSearchModel.savedSearchesArticleTitle.getByText(SavedSearchPageConstants.searchTerm + " saved search_" + randomNumber).first()).toBeVisible();
+    await expect(savedSearchModel.savedSearchesArticleTitle.getByText(SavedSearchPageConstants.searchTerm + savedSearchNameSuffix).first()).toBeVisible();
 
     await expect(savedSearchModel.savedSearchesArticleDeleteButton.first()).toBeVisible();
     await savedSearchModel.savedSearchesArticleDeleteButton.first().click();
 
     //initial saved search not visible now
-    await expect(savedSearchModel.savedSearchesArticleTitle.getByText(SavedSearchPageConstants.searchTerm + " saved search_" + randomNumber).first()).not.toBeVisible();
+    await expect(page.getByText(SavedSearchPageConstants.searchTerm + savedSearchNameSuffix)).not.toBeVisible();
   });
 
-
   test("Can undo a deleted saved search", async ({ page, savedSearchModel }) => {
+    await page.goto("/search");
+    await expect(savedSearchModel.searchInput).toBeVisible();
+    await savedSearchModel.searchInput.fill(SavedSearchPageConstants.searchTerm);
+    await savedSearchModel.searchButton.click();
+
+    //save the search using the modal
+    //click saved search button
+    await expect(savedSearchModel.saveSearchButton).toBeVisible();
+    await savedSearchModel.saveSearchButton.click();
+
+    let savedSearchNameSuffix = " saved search_" + Math.random().toString().substring(2);
+    //see saved search modal, enter text and click Save button
+    await expect(savedSearchModel.savedSearchModal).toBeVisible();
+    await savedSearchModel.savedSearchInput.fill(SavedSearchPageConstants.searchTerm + savedSearchNameSuffix);
+    await page.getByRole('button').getByText('Save').click();
+
     await page.goto("/mycru");
     await expect(savedSearchModel.tabSearches).toBeVisible();
     await savedSearchModel.tabSearches.click();
     await expect(savedSearchModel.savedSearchesList).toBeVisible();
 
-    let randomNumber = Math.random().toString().substring(2);
     //expect search item to be visible
-    await expect(savedSearchModel.savedSearchesArticleTitle.getByText(SavedSearchPageConstants.searchTerm + " saved search_" + randomNumber).first()).toBeVisible();
+    await expect(savedSearchModel.savedSearchesArticleTitle.getByText(SavedSearchPageConstants.searchTerm + savedSearchNameSuffix).first()).toBeVisible();
 
     await expect(savedSearchModel.savedSearchesArticleDeleteButton.first()).toBeVisible();
     await savedSearchModel.savedSearchesArticleDeleteButton.first().click();
@@ -138,7 +176,14 @@ test.describe("Searches and MyCru Page", () => {
     await savedSearchModel.deletedSavedSearchUndoButton.getByRole('button').first().click();
 
     //expect search item to be visible
-    await expect(savedSearchModel.savedSearchesArticleTitle.getByText(SavedSearchPageConstants.searchTerm + " saved search_" + randomNumber).first()).toBeVisible();
+    await expect(savedSearchModel.savedSearchesArticleTitle.getByText(SavedSearchPageConstants.searchTerm + savedSearchNameSuffix).first()).toBeVisible();
+
+    //clean up the saved search
+    await expect(savedSearchModel.savedSearchesArticleDeleteButton.first()).toBeVisible();
+    await savedSearchModel.savedSearchesArticleDeleteButton.first().click();
+    await expect(page.getByText(SavedSearchPageConstants.searchTerm + savedSearchNameSuffix)).not.toBeVisible();
   });
 });
+
+
 
