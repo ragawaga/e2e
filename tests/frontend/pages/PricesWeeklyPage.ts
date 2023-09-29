91953ca6047a6a04d9112ee132a2d115c20c13e8
Bryan Kerr <btkerr13@gmail.com>
2023-09-29 11:43:21 +0100

    PR feedback

--- tests/frontend/pages/PricesWeeklyPage.ts
+++ tests/frontend/pages/PricesWeeklyPage.ts
@@ -1,9 +1,16 @@
 import { Page } from "@playwright/test";
 import { createComponentLocators } from "../../component";
 
+export const pricesWeeklyConstants = {
+  myPricesTab: "/mycru?tab=my-prices",
+};
+
 export function pricesWeeklyPageModel(page: Page) {
+  const priceId = 1001;
   const screen = createComponentLocators(page, {
     aggridPricesTable: ".ag-theme-material.prices-table",
+    priceBookmarkButton: `button[data-testId="bookmark-price-button"][data-articleid="${priceId}"]`,
+    bookmarkedPrice: `a[href="/price/${priceId}"]`,
   });
 
   return {
--- tests/frontend/pricesBookmarks.spec.ts
+++ tests/frontend/pricesBookmarks.spec.ts
@@ -1,12 +1,15 @@
 import { expect } from "@playwright/test";
+import { GlobalConstants } from "./pages/Layout";
 
 import { createTestFixture } from "../fixture";
 
-import { pricesWeeklyPageModel } from "./pages/PricesWeeklyPage";
+import {
+  pricesWeeklyConstants,
+  pricesWeeklyPageModel,
+} from "./pages/PricesWeeklyPage";
 
 const test = createTestFixture("pricesWeekly", pricesWeeklyPageModel);
 
-const priceId = 1001;
 const timeout = 12000;
 
 test.describe("Prices bookmarking @unrestricted", () => {
@@ -22,37 +25,27 @@ test.describe("Prices bookmarking @unrestricted", () => {
         timeout: timeout,
       };
 
-    // Get the aluminium price bookmark button.
-    const priceBookmarkButton = pricesWeekly.page.locator(
-      `button[data-testId="bookmark-price-button"][data-articleid="${priceId}"]`
-    );
-
     // Check if price is already bookmarked.
-    const isBookmarked = await priceBookmarkButton.getAttribute(
-      "data-selected"
+    const isBookmarked = await pricesWeekly.priceBookmarkButton.getAttribute(
+      GlobalConstants.isSelectedAttribute
     );
 
     // If price is bookmarked, click it to unbookmark.
     // eslint-disable-next-line playwright/no-conditional-in-test
     if (isBookmarked === "true") {
-      await priceBookmarkButton.click();
+      await pricesWeekly.priceBookmarkButton.click();
     }
 
     // Click to bookmark price.
-    await priceBookmarkButton.click();
+    await pricesWeekly.priceBookmarkButton.click();
 
     // Navigate to mycru my-prices tab.
-    await pricesWeekly.page.goto("/mycru?tab=my-prices");
-
-    // Check if an anchor is displayed in relation to the bookmarked price.
-    const bookmarkedPrice = pricesWeekly.page.locator(
-      `a[href="/price/${priceId}"]`
-    );
+    await pricesWeekly.page.goto(pricesWeeklyConstants.myPricesTab);
 
     // Wait for the anchor to appear.
-    await bookmarkedPrice.waitFor({ state: "visible", timeout });
+    await pricesWeekly.bookmarkedPrice.waitFor({ state: "visible", timeout });
 
     // Check that the bookmarked price is visible.
-    await expect(bookmarkedPrice).toBeVisible();
+    await expect(pricesWeekly.bookmarkedPrice).toBeVisible();
   });
 });
